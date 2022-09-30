e941a65b7c6c06a178b3efa2eaf290115cac4f4c
Chantal Hodge <chantal_h09@yahoo.com>
2022-09-29 20:45:32 -0500

    index page formatted to move on to js page to link

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,5 @@ var pastSearchButtonEl = document.querySelector("#past-search-buttons");
 
 var getCityWeather = function(city){
     var apiKey = "6eb39225f19081d854391179d7843a68"
-    var apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
+    var apiURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}"
+}
