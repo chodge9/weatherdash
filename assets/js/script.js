c1168767a83c021f98c93fa43bab99b5eef4f0ea
Chantal Hodge <chantal_h09@yahoo.com>
2022-09-29 20:07:48 -0500

    initial code set up with js and css pages

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var cities = [];
+var citySearchInputEl = document.querySelector("#searched-city");
+var weatherContainerEl=document.querySelector("#current-weather-container");
+var cityFormEl=document.querySelector("#city-search-form");
+var cityInputEl=document.querySelector("#city");
+var forecastTitle = document.querySelector("#forecast");
+var forecastContainerEl = document.querySelector("#fiveday-container");
+var pastSearchButtonEl = document.querySelector("#past-search-buttons");
+
+
+var getCityWeather = function(city){
+    var apiKey = "6eb39225f19081d854391179d7843a68"
+    var apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
